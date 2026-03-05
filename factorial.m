6e88ce45268eaf6aedd690a94f6b2bc405e87f08
fsevillanob <fsevillanob@alumni.unav.es>
2026-03-05 17:08:53 +0100

    Fix factorial calculation and improve input prompt

--- factorial.m
+++ factorial.m
@@ -1,16 +1,16 @@
 clear all
 clc
 
-num=input('Introduzca un número entero:);
+num=input('Introduzca un número entero, no negativo: ');
 
-factorial=1
+factorial=1;
 
-for i=1:num-1
-    factorial=factorial*i
+for i=1:num
+    factorial=factorial*i;
 end
 
 if num>=0
-    disp('El factorial de' num2str(num) 'es:' factorial)
+    disp(['El factorial de ', num2str(num), ' es: ', num2str(factorial_val)])
 else
     disp(['El factorial de',num,'es:',factorial])
 end
